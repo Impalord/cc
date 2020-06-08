3bb4720154953ed474293477b09d0ccea9cd6216
Impalord <13741@student.vgu.edu.vn>
2020-06-08 14:54:13 +0700

    Final

--- publisher2.js
+++
@@ -1,5 +0,0 @@
-var mqtt = require('mqtt')
-var client = mqtt.connect('mqtt://test.mosquitto.org')
-client.on('connect', function () {
-setInterval(function(){client.publish('now', 'Hello mqtt')},1000)
-})
